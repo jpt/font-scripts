2b191fa3bb20b21032ac800dd132e0142afdf51c
Jeremy Tribby <jeremy@tribby.com>
2017-03-11 22:28:20 -0800

    finish script

--- Glyphs/Sidebearings Brace Layer Fix.py
+++ Glyphs/Sidebearings Brace Layer Fix.py
@@ -11,9 +11,27 @@ brace = re.compile("^{\d+,\s*\d+}$")
 
 Font = Glyphs.font
 
+
+print Font.glyphs['a'].layers
+
+
 for glyph in Font.glyphs:
   if glyph.layers > 1 and '.' not in glyph.name:
     for layer in glyph.layers:
       if brace.match(layer.name):
-        print layer.name
-        print layer.LSB
+
+        name = layer.name
+
+        newLayer = layer.copy()
+        layer.name = "tmp"
+        newLayer.name = name
+
+        glyph.layers.append(newLayer)
+
+        newLayer.reinterpolate()
+
+        layer.LSB = newLayer.LSB
+        layer.RSB = newLayer.RSB
+        layer.name = name
+
+        del(glyph.layers[newLayer.layerId])

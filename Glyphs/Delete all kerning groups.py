1a3bc624489feb2e04c7f760ef03fcf395623a03
Jeremy Tribby <jeremy@tribby.com>
2021-06-24 02:14:01 -0700

    delete all kerning

--- Glyphs/Delete all kerning groups.py
+++ Glyphs/Delete all kerning groups.py
@@ -1,17 +1,29 @@
-#MenuTitle: Delete all kerning groups
+#MenuTitle: Delete kerning groups and values
 # -*- coding: utf-8 -*-
 __doc__="""
-Delete all kerning groups across all masters
+Delete all kerning groups and values across all masters
 """
-c = 0
+
+Font = Glyphs.font
+gc = 0
+
+def deleteGroup(direction):
+    gc += 1
+    print("Deleting %s kerning group for glyph: %s" % (direction, glyph.name))
+    glyph[direction + "KerningGroup"] = None
+
 for glyph in Font.glyphs:
     for layer in glyph.layers:
         if glyph.rightKerningGroup:
-            c += 1
-            print("Deleting right kerning group for glyph: %s" % glyph.name)
-            glyph.rightKerningGroup = None
+            deleteGroup("right")
         if glyph.leftKerningGroup:
-            c += 1
-            print("Deleting left kerning group for glyph: %s" % glyph.name)
-            glyph.leftKerningGroup = None
-print("\nDone! Deleted %s groups in %s masters." % (c, len(Font.masters)))
+            deleteGroup("left")
+        if glyph.topKerningGroup:
+            deleteGroup("top")
+        if glyph.bottomKerningGroup:
+            deleteGroup("bottom")
+
+Font.kerning = {}
+Font.kerningRTL = {}
+Font.kerningVertical = {}
+print("\nDone! Deleted %s groups and all kerning in %s masters." % (gc, len(Font.masters)))

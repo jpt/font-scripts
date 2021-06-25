bfb9f2b6cfa53672a77e7effa37f8930d8dbac62
Jeremy Tribby <jeremy@tribby.com>
2021-06-24 20:07:48 -0700

    be explicit about beta status

--- Glyphs/Export UFO and Designspace files.py
+++ Glyphs/Export UFO and Designspace files.py
@@ -1,4 +1,4 @@
-# MenuTitle: Export UFO and Designspace files
+# MenuTitle: Export UFO and Designspace files (Beta)
 __doc__ = """
 Export UFO and designspace files. Supports axis mappings and brace layers.  
 """

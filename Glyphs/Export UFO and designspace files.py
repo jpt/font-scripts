d775a07909654be001790f0ef65691fa076f26bb
Jeremy Tribby <jeremy@tribby.com>
2021-11-08 23:08:45 -0800

    remove kerning from brace layers

--- Glyphs/Export UFO and designspace files.py
+++ Glyphs/Export UFO and designspace files.py
@@ -12,7 +12,6 @@ from fontTools.designspaceLib import (
 	DesignSpaceDocument, AxisDescriptor, SourceDescriptor, InstanceDescriptor, RuleDescriptor)
 
 is_vf = True  # todo add vanilla interface for this
-delete_unnecessary_glyphs = True # set to false if you want to keep kerning for now... 
 
 def getMutedGlyphs(font):
 	__doc__ = "Returns an array of non-exporting glyphs to be added as muted glyphs in the designspace"
@@ -332,7 +331,6 @@ def getDesignSpaceDocument(font):
 
 
 def generateMastersAtBraces(font, temp_ufo_folder):
-	global delete_unnecessary_glyphs
 	special_layer_axes = getSpecialLayerAxes(font)
 	for instance in font.instances:
 		print(instance.axes)
@@ -344,16 +342,27 @@ def generateMastersAtBraces(font, temp_ufo_folder):
 		ufo_file_name = "%s.ufo" % ins.name
 		ins.axes = axes
 		brace_font = ins.interpolatedFont
-		if delete_unnecessary_glyphs:
-			glyph_names_to_delete = getNonSpecialGlyphs(font, axes)
-			for glyph in glyph_names_to_delete:
-				del(brace_font.glyphs[glyph])
-			feature_keys = [feature.name for feature in brace_font.features]
-			for key in feature_keys:
-				del(brace_font.features[key])
-			class_keys = [font_class.name for font_class in brace_font.classes]
-			for key in class_keys:
-				del(brace_font.classes[key])
+		glyph_names_to_delete = getNonSpecialGlyphs(font, axes)
+		for glyph in glyph_names_to_delete:
+			del(brace_font.glyphs[glyph])
+		feature_keys = [feature.name for feature in brace_font.features]
+		for key in feature_keys:
+			del(brace_font.features[key])
+		class_keys = [font_class.name for font_class in brace_font.classes]
+		for key in class_keys:
+			del(brace_font.classes[key])
+		for glyph in brace_font.glyphs:
+			if glyph.rightKerningGroup:
+				glyph.rightKerningGroup = None
+			if glyph.leftKerningGroup:
+				glyph.leftKerningGroup = None
+			if glyph.topKerningGroup:
+				glyph.topKerningGroup = None
+			if glyph.bottomKerningGroup:
+				glyph.bottomKerningGroup = None
+		brace_font.kerning = {}
+		brace_font.kerningRTL = {}
+		brace_font.kerningVertical = {}
 		ufo_file_path = os.path.join(temp_ufo_folder, ufo_file_name)
 		exportSingleUFObyMaster(brace_font.masters[0], ufo_file_path)
 

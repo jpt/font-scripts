f3ed859188a32ce5e7d04f88f461454a0a0aa793
Jeremy Tribby <jeremy@tribby.com>
2022-04-04 22:30:43 -0700

    rm print

--- Glyphs/Add Axis Mappings from Axis Locations.py
+++ Glyphs/Add Axis Mappings from Axis Locations.py
@@ -8,7 +8,6 @@ def getAxisMappings(F):
 	for instance in F.instances:
 		if instance.type == 0:
 			for i,internal in enumerate(instance.axes):
-				#print(F.customParameters["Axis Mappings"]["wght"])
 				external = instance.customParameters["Axis Location"][i]['Location']
 				axis_tag = F.axes[i].axisTag
 				try:
@@ -20,7 +19,6 @@ def getAxisMappings(F):
 
 F = Glyphs.font
 axis_map = getAxisMappings(F)
-print(axis_map)
 if not F.customParameters["Axis Mappings"]:
 	F.customParameters["Axis Mappings"] = dict()
 F.customParameters["Axis Mappings"] = axis_map

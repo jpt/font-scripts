24c293279fa19b3787f814036ef3f8dbb43c5376
Jeremy Tribby <jeremy@tribby.com>
2023-11-23 18:58:19 -0800

    fix centering

--- Glyphs/Centering Tool.py
+++ Glyphs/Centering Tool.py
@@ -1,12 +1,13 @@
-#MenuTitle: Centering Tools
+# MenuTitle: Centering Tools
 # -*- coding: utf-8 -*-
 
-__doc__="""
+__doc__ = """
 GUI for various centering tools: horizontally, vertically in x-height, ascender height, and cap height.
 """
 
 import vanilla
 from Foundation import NSPoint
+
 
 class CenteringToolUI:
     def __init__(self):
@@ -16,33 +17,68 @@ class CenteringToolUI:
         bottom_padding = 16
         button_height = 20
         button_spacing = 8
-        button_titles = ["Horizontally center", "Vertically center in x-height", "Vertically center in capheight", "Vertically center in ascender height"]
-        actions = [self.centerHorizontally, self.centerXHeight, self.centerCapHeight, self.centerAscenderHeight]
+        button_titles = [
+            "Horizontally center",
+            "Vertically center in x-height",
+            "Vertically center in capheight",
+            "Vertically center in ascender height",
+        ]
+        actions = [
+            self.centerHorizontally,
+            self.centerXHeight,
+            self.centerCapHeight,
+            self.centerAscenderHeight,
+        ]
 
-        # Calculate the total window height
-        total_height = top_padding + bottom_padding + button_height * len(button_titles) + button_spacing * (len(button_titles) - 1)
+        total_height = (
+            top_padding
+            + bottom_padding
+            + button_height * len(button_titles)
+            + button_spacing * (len(button_titles) - 1)
+        )
 
         self.w = vanilla.FloatingWindow((300, total_height), "Centering Tool")
-        
-        y = top_padding  # Initial Y position with top padding
+
+        y = top_padding
 
         for title, action in zip(button_titles, actions):
-            setattr(self.w, title, vanilla.Button((horizontal_padding, y, -horizontal_padding, button_height), title, callback=action))
+            setattr(
+                self.w,
+                title,
+                vanilla.Button(
+                    (horizontal_padding, y, -horizontal_padding, button_height),
+                    title,
+                    callback=action,
+                ),
+            )
             y += button_height + button_spacing
 
         self.w.open()
 
-    def get_combined_bounds(self, selected_paths, selected_components):
-        combined_min_x, combined_max_x, combined_min_y, combined_max_y = None, None, None, None
+    def get_combined_bounds(self, selected_paths, selected_components, selected_anchors):
+        combined_min_x, combined_max_x = float('inf'), float('-inf')
+        combined_min_y, combined_max_y = float('inf'), float('-inf')
+
         for item in selected_paths + selected_components:
-            if combined_min_x is None or item.bounds.origin.x < combined_min_x:
-                combined_min_x = item.bounds.origin.x
-            if combined_max_x is None or (item.bounds.origin.x + item.bounds.size.width) > combined_max_x:
-                combined_max_x = item.bounds.origin.x + item.bounds.size.width
-            if combined_min_y is None or item.bounds.origin.y < combined_min_y:
-                combined_min_y = item.bounds.origin.y
-            if combined_max_y is None or (item.bounds.origin.y + item.bounds.size.height) > combined_max_y:
-                combined_max_y = item.bounds.origin.y + item.bounds.size.height
+            min_x = item.bounds.origin.x
+            max_x = item.bounds.origin.x + item.bounds.size.width
+            min_y = item.bounds.origin.y
+            max_y = item.bounds.origin.y + item.bounds.size.height
+
+            combined_min_x = min(combined_min_x, min_x)
+            combined_max_x = max(combined_max_x, max_x)
+            combined_min_y = min(combined_min_y, min_y)
+            combined_max_y = max(combined_max_y, max_y)
+
+        for anchor in selected_anchors:
+            anchor_x = anchor.position.x
+            anchor_y = anchor.position.y
+
+            combined_min_x = min(combined_min_x, anchor_x)
+            combined_max_x = max(combined_max_x, anchor_x)
+            combined_min_y = min(combined_min_y, anchor_y)
+            combined_max_y = max(combined_max_y, anchor_y)
+
         return combined_min_x, combined_max_x, combined_min_y, combined_max_y
 
     def center_selection(self, horizontal=False, vertical=False, height=None):
@@ -50,14 +86,17 @@ class CenteringToolUI:
         selected_components = [comp for comp in self.font.selectedLayers[0].components if comp.selected]
         selected_anchors = [anchor for anchor in self.font.selectedLayers[0].anchors if anchor.selected]
 
-        if not selected_paths and not selected_components:
-            print("No paths or components selected.")
+        if not selected_paths and not selected_components and not selected_anchors:
+            print("No paths, components, or anchors selected.")
             return
 
-        combined_min_x, combined_max_x, combined_min_y, combined_max_y = self.get_combined_bounds(selected_paths, selected_components)
+        include_anchors_in_bounds = not selected_paths and not selected_components
+        combined_min_x, combined_max_x, combined_min_y, combined_max_y = self.get_combined_bounds(
+            selected_paths, selected_components, selected_anchors if include_anchors_in_bounds else []
+        )
 
-        move_x = (self.font.selectedLayers[0].width/2 - (combined_max_x - combined_min_x)/2 - combined_min_x) if horizontal else 0
-        move_y = ((height/2 - (combined_max_y - combined_min_y)/2 - combined_min_y) if height else 0) if vertical else 0
+        move_x = (self.font.selectedLayers[0].width / 2 - (combined_max_x + combined_min_x) / 2) if horizontal and combined_min_x is not None else 0
+        move_y = (height / 2 - (combined_max_y + combined_min_y) / 2) if vertical and combined_min_y is not None else 0
 
         for item in selected_paths + selected_components:
             item.applyTransform((1.0, 0.0, 0.0, 1.0, move_x, move_y))
@@ -69,12 +108,18 @@ class CenteringToolUI:
         self.center_selection(horizontal=True)
 
     def centerXHeight(self, sender):
-        self.center_selection(vertical=True, height=self.font.selectedLayers[0].master.xHeight)
+        self.center_selection(
+            vertical=True, height=self.font.selectedLayers[0].master.xHeight
+        )
 
     def centerAscenderHeight(self, sender):
-        self.center_selection(vertical=True, height=self.font.selectedLayers[0].master.ascender)
+        self.center_selection(
+            vertical=True, height=self.font.selectedLayers[0].master.ascender
+        )
 
     def centerCapHeight(self, sender):
-        self.center_selection(vertical=True, height=self.font.selectedLayers[0].master.capHeight)
+        self.center_selection(
+            vertical=True, height=self.font.selectedLayers[0].master.capHeight
+        )
 
 CenteringToolUI()

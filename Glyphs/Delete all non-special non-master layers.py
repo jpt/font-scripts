250fa3889077cffa921b228df5a3a89ae7f3e8c8
Jeremy Tribby <jeremy@tribby.com>
2023-12-14 17:00:15 -0800

    fix deletion

--- Glyphs/Delete all non-special non-master layers.py
+++ Glyphs/Delete all non-special non-master layers.py
@@ -1,11 +1,17 @@
 #MenuTitle: Delete all non-special non-master layers
 # -*- coding: utf-8 -*-
 
-__doc__="""
+__doc__ = """
 Deletes all non-special (brace, bracket), non-master layers in the current font.
 """
 
 for glyph in Glyphs.font.glyphs:
-	for i,layer in enumerate(glyph.layers):
-		if not layer.isSpecialLayer and not layer.isMasterLayer:
-			print("Deleting %s in %s" % (layer.name, layer.parent.name))
+    layers_to_delete = []
+
+    for layer in glyph.layers:
+        if not layer.isSpecialLayer and not layer.isMasterLayer:
+            layers_to_delete.append(layer.layerId)
+
+    for layerId in layers_to_delete:
+        print(f"Deleting layerId {layerId} ({layer.name}) in {glyph.name}")
+        del glyph.layers[layerId]
